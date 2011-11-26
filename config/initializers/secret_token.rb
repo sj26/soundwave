3c430f6e6dd1fc9d1e1da3d47f942043f6d4c771
Samuel Cochran <sj26@sj26.com>
2011-11-26 20:53:32 +0800

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundwave::Application.config.secret_token = '778789573aeedbb5396956f97715809ef5cc95d61f267ddf5a7f4de55eda30718048077336efa1729ef270404e95c604b29554c55ab51f9d331c926693b652ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Soundwave::Application.config.session_store :cookie_store, key: '_Soundwave_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Soundwave::Application.config.session_store :active_record_store
