2617c5e4fafcae53e829c76cd65a8002d18cff8c
Samuel Cochran <sj26@sj26.com>
2011-11-27 11:05:26 +0800

    Add waveform version generation

--- app/uploaders/sound_file_uploader.rb
+++ app/uploaders/sound_file_uploader.rb
@@ -8,4 +8,21 @@ class SoundFileUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(ogg mp3 m4a)
   end
+
+  version :waveform do
+    process :convert_to_waveform
+
+    def convert_to_waveform
+      cache_stored_file! if !cached?
+      Dir::Tmpname.create(File.basename(current_path)) do |tempname|
+        begin
+          system %Q{ffmpeg -y -i "#{current_path}" -f wav "#{tempname}" > /dev/null 2>&1}
+          FileUtils.rm current_path
+          Waveform.new(tempname).generate(current_path)
+        ensure
+          FileUtils.rm tempname
+        end
+      end
+    end
+  end
 end
